1ccc797964d6c0bdd4e8ade5db0b2991b3c0a033
vvvladislav <vladislavvaliuchenkov@gmail.com>
2023-03-07 05:19:31 -0800

    added app placeholder removal util

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,10 @@
 import { render } from "preact";
 
+import { removePlaceholder } from "@utils/removePlaceholder";
+
 import { App } from "./layout/App";
 
+
+removePlaceholder();
 
 render(<App/>, document.getElementById("app") as HTMLDivElement);
---
+++ src/utils/removePlaceholder.ts
@@ -0,0 +1,4 @@
+export function removePlaceholder(): void {
+  const placeholder = document.getElementById("placeholder") as HTMLElement;
+  placeholder.remove();
+}
