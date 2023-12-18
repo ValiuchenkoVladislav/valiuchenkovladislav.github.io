ce697f0ce017740d3dcaa52e4b92c776ecdfa65e
Vladislav V <vladislavvaliuchenkov@gmail.com>
2023-12-18 15:31:59 -0500

    added github logo icon

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,6 +1,8 @@
 import { StaticUIBar } from "~/components/StaticUIBar";
 import { ScopeCorner } from "~/components/ScopeCorner";
 
+import { ReactComponent as GitHubLogo } from "~/assets/images/githubLogo.svg";
+
 
 export function Footer() {
   return (
@@ -12,7 +14,8 @@ export function Footer() {
           duration-700 opacity-75 hover:opacity-100
         `}
       >
-        aa
+        <GitHubLogo className="w-[3rem] aspect-square"/>
+
         <span className="text-[1.4rem] max-[352px]:hidden">
           @VALIUCHENKOVLADISLAV
         </span>
